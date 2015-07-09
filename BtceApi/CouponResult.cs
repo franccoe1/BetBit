6d790d8be68a46f2648e4832f673c27a235cc04f
Francisco Coelho <franc.coe1@gmail.com>
2015-07-09 15:32:18 +0100

    first commit

---
+++ BtceApi/CouponResult.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace BtceApi
+{
+    public class CouponResult
+    {
+        public Funds Funds { get; private set; }
+        public int CouponAmount { get; private set; }
+        public string CouponCurrency { get; private set; }
+        public int TransID { get; private set; }
+
+        private CouponResult() { }
+        public static CouponResult ReadFromJObject(JObject o)
+        {
+            return new CouponResult()
+            {
+                Funds = Funds.ReadFromJObject(o["funds"] as JObject),
+                CouponAmount = o.Value<int>("couponAmount"),
+                CouponCurrency = o.Value<string>("couponCurrency"),
+                TransID = o.Value<int>("transID")
+            };
+        }
+    }
+}
