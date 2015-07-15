985116a43c41aced8553ddb679961e4104d33cc9
Francisco Coelho <franc.coe1@gmail.com>
2015-07-15 18:57:46 +0100

    login logout

--- BetBit.Frontend/Controllers/AccountController.cs
+++ BetBit.Frontend/Controllers/AccountController.cs
@@ -57,7 +57,7 @@ namespace BetBit.Frontend.Controllers
             return user;
         }
 
-        public JsonResult UserLogin(string username, string password)
+        public JsonResult UserLogin(string username, string password, string logout)
         {
             BetBitEntities betBitEntities = new BetBitEntities();
             HttpCookie myCookie = Request.Cookies["BetBit"];
@@ -76,6 +76,53 @@ namespace BetBit.Frontend.Controllers
                 user.Password = User.Password;
                 user.UserId = User.UserId.Value;
             }
+            if (logout.Equals("true"))
+            {
+                Response.Cookies["BetBit"].Expires = DateTime.Now.AddDays(-1);
+                myCookie = null;
+
+                if (myCookie == null)
+                    return Logout();
+            }
+            return Json(user, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult Logout()
+        {
+            User user = new User();
+            HttpCookie myCookie = null;
+
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+            var random = new Random();
+            user.Username = new string(
+                Enumerable.Repeat(chars, 8)
+                          .Select(s => s[random.Next(s.Length)])
+                          .ToArray());
+
+            user.Password = new string(
+                Enumerable.Repeat(chars, 12)
+                          .Select(s => s[random.Next(s.Length)])
+                          .ToArray());
+
+            user.UserId = Guid.NewGuid();
+
+            myCookie = new HttpCookie("BetBit");
+            myCookie.Value = user.UserId.ToString();
+            myCookie.Expires = DateTime.Now.AddYears(1);
+            Response.Cookies.Add(myCookie);
+
+            BetBitEntities betBitEntities = new BetBitEntities();
+
+            betBitEntities.Users.Add(new Users()
+            {
+                UserId = user.UserId,
+                Username = user.Username,
+                Password = user.Password
+            });
+
+            betBitEntities.SaveChanges();
+
             return Json(user, JsonRequestBehavior.AllowGet);
         }
 
